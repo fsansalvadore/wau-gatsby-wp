6c4e0f4bcb955b75eb855b46bd600218ac3095a2
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2021-01-30 23:20:23 +0100

    en/ fix

--- src/components/LayoutComponent.js
+++ src/components/LayoutComponent.js
@@ -10,7 +10,7 @@ const Layout = ({ isMenuLight, children }) => {
   let lang = "it"
   if (typeof window !== `undefined`) {
       location = window.location.href
-      if(location.includes("00/en")) {
+      if(location.includes("00/en") || location.includes("app/en") || location.includes("com/en")) {
         lang = "en"
       }
   }
