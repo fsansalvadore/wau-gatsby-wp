52d0975dbcc703a6b98f94714b0f3bef3dbab7b9
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2021-04-30 18:18:24 +0200

    feat(analytics): added google analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-178906465-1",
+        trackingId: "UA-195952687-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
