c9cdf74590a1fcde30e89a7654a9ff97c4fc0db8
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2021-05-12 09:43:09 +0200

    fix(referenti): correct referenti for each lang

--- src/pages/team.en.js
+++ src/pages/team.en.js
@@ -37,7 +37,7 @@ const TeamPageIta = () => {
             }
           }
         }
-        collaborators(first: 100, where: { status: PUBLISH }) {
+        collaborators(first: 100, where: { status: PUBLISH, language: EN }) {
           nodes {
             date
             title
