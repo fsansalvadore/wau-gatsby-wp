ee2b831c66c93ad89e8a1b445bdf826903ef7115
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2021-05-19 08:58:24 +0200

    forms: forms fixxxy

--- src/pages/wau-camp-form-test.it.js
+++ src/pages/wau-camp-form-test.it.js
@@ -2,7 +2,25 @@ import React from "react";
 import WauCampContactForm from "../components/elements/WauCamp/WauCampContactForm";
 
 const WauCampItaFormTest = () => {
-  return <WauCampContactForm lang="it" />;
+  return (
+    <>
+      <WauCampContactForm lang="it" />
+      <form
+        name="waucamp"
+        method="POST"
+        type="hidden"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="text" name="nome" required />
+        <input type="text" name="cognome" required />
+        <input type="email" name="email" required />
+        <input type="text" name="nascita" />
+        <textarea name="messaggio" required />
+        <button type="submit">Invia</button>
+      </form>
+    </>
+  );
 };
 
 // eslint-disable-next-line import/no-default-export
