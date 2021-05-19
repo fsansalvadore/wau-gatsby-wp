752d327ba283527ea46ac7a6dacf064ab3c32d9e
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2021-05-19 08:55:54 +0200

    forms: forms fixxxy

---
+++ src/pages/wau-camp-form-test.it.js
@@ -0,0 +1,9 @@
+import React from "react";
+import WauCampContactForm from "../components/elements/WauCamp/WauCampContactForm";
+
+const WauCampItaFormTest = () => {
+  return <WauCampContactForm lang="it" />;
+};
+
+// eslint-disable-next-line import/no-default-export
+export default WauCampItaFormTest;
